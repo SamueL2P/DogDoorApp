892f556f5062390147a594c1f70b264820f93982
Samuel <padmadansamuel@gmail.com>
2024-08-14 20:05:32 +0530

    Add project files.

---
+++ Models/DogDoor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Timer = System.Timers.Timer;
+
+namespace DogDoorApp.Models
+{
+    internal class DogDoor
+    {
+        private bool open;
+        private Timer timer;
+
+        public DogDoor() {
+            this.open = false;
+        }
+
+        public void Open()
+        {
+            Console.WriteLine("The Dog Door Opens");
+            open = true;
+
+            Timer timer = new Timer(5000);
+            timer.Elapsed += (sender, e) => Close();
+            timer.AutoReset = false;
+            timer.Start();
+        }
+
+        public void Close()
+        {
+            open = false;
+            Console.WriteLine("The Dog Door Closes");
+        }
+
+        public bool IsOpen() { 
+            return open;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using DogDoorApp.Models;
+
+namespace DogDoorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DogDoor door = new DogDoor();
+            Remote remote = new Remote(door);
+
+            Console.WriteLine("Fido starts barking.");
+            remote.PressButton();
+
+            Console.WriteLine("\nFido has gone outside...");
+            Console.WriteLine("\nFido's all done...");
+
+            Thread.Sleep(10000);
+            Console.WriteLine("\nBut he is stuck outside");
+
+            Console.WriteLine("Fido starts barking...");
+            Console.WriteLine("So Grab the Controller");
+            remote.PressButton();
+
+            Console.WriteLine("\nFido's back inside...");
+            Console.ReadLine();
+        }
+    }
+}
